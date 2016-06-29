50b35605654d466456d3771b32f98ec7d60a648d
abhirammallatta <p14abhiramts@iimahd.ernet.in>
2016-06-29 15:56:13 +0530

    work still going on

--- indexlockfurniture.php
+++ indexlockfurniture.php
@@ -293,7 +293,7 @@
                     </div>
                 </div>       
                 <div class="col-md-4"> 
-                         <div class="card lock">
+                         <div class="card lock" data-toggle="modal" data-target="#myModal">
                             <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                             
                             Click to see the card
@@ -302,21 +302,21 @@
             </div>
             <div class="row">
                     <div class="col-md-4"> 
-                         <div class="card lock">
+                         <div class="card lock" data-toggle="modal" data-target="#myModal">
                             <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                             
                             Click to see the card
                         </div>
                     </div>
                      <div class="col-md-4"> 
-                        <div class="card lock">
+                        <div class="card lock" data-toggle="modal" data-target="#myModal">
                             <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                             
                             Click to see the card
                         </div>
                     </div>
                      <div class="col-md-4"> 
-                        <div class="card lock">
+                        <div class="card lock" data-toggle="modal" data-target="#myModal">
                             <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                             
                             Click to see the card
@@ -325,21 +325,21 @@
                 </div>  
             <div class="row">
                     <div class="col-md-4"> 
-                         <div class="card lock">
+                         <div class="card lock" data-toggle="modal" data-target="#myModal">
                             <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                             
                             Click to see the card
                         </div>
                     </div>
                      <div class="col-md-4"> 
-                        <div class="card lock">
+                        <div class="card lock" data-toggle="modal" data-target="#myModal">
                             <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                             
                             Click to see the card
                         </div>
                     </div>
                      <div class="col-md-4"> 
-                        <div class="card lock">
+                        <div class="card lock" data-toggle="modal" data-target="#myModal">
                             <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                             
                             Click to see the card
@@ -388,7 +388,7 @@
                 </div>
             </div>
              <div class="col-md-4"> 
-                     <div class="card lock">
+                     <div class="card lock" data-toggle="modal" data-target="#myModal">
                         <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                         
                         Click to see the card
@@ -397,21 +397,21 @@
             </div>
             <div class="row">
                 <div class="col-md-4"> 
-                     <div class="card lock">
+                     <div class="card lock" data-toggle="modal" data-target="#myModal">
                         <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                         
                         Click to see the card
                     </div>
                 </div>
                  <div class="col-md-4"> 
-                    <div class="card lock">
+                    <div class="card lock" data-toggle="modal" data-target="#myModal">
                         <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                         
                         Click to see the card
                     </div>
                 </div>
                  <div class="col-md-4"> 
-                    <div class="card lock">
+                    <div class="card lock" data-toggle="modal" data-target="#myModal">
                         <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                         
                         Click to see the card
@@ -420,21 +420,21 @@
             </div>  
             <div class="row">
                 <div class="col-md-4"> 
-                     <div class="card lock">
+                     <div class="card lock" data-toggle="modal" data-target="#myModal">
                         <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                         
                         Click to see the card
                     </div>
                 </div>
                  <div class="col-md-4"> 
-                    <div class="card lock">
+                    <div class="card lock" data-toggle="modal" data-target="#myModal">
                         <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                         
                         Click to see the card
                     </div>
                 </div>
                  <div class="col-md-4"> 
-                    <div class="card lock">
+                    <div class="card lock" data-toggle="modal" data-target="#myModal">
                         <h1><span class="fa fa-lock" aria-hidden="true"></span></h1>
                         
                         Click to see the card
@@ -443,7 +443,24 @@
             </div>
        
      
-       
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                 <h1>Spottr</h1>
+                <h4 class="modal-title">Discover your next favorite service</h4>
+                 <br>
+                 <br>
+                <div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="false" data-auto-logout-link="true" onlogin="checkLoginState();">Continue with Facebook</div>
+
+            </div> 
+        </div>
+    </div>
+</div>
+</div>
 
         <script>
                 /**
@@ -502,6 +519,79 @@
                 });
             </script>
 
+
+
+            <script>
+                function statusChangeCallback(response) {
+                    console.log('statusChangeCallback');
+                    console.log(response);
+                    // The response object is returned with a status field that lets the
+                    // app know the current login status of the person.
+                    // Full docs on the response object can be found in the documentation
+                    // for FB.getLoginStatus().
+                    if (response.status === 'connected') {
+                      testAPI();
+                    } else if (response.status === 'not_authorized') {
+                      // The person is logged into Facebook, but not your app.
+                      document.getElementById('status').innerHTML = 'Please log ' +
+                        'into this app.';
+                    } else {
+                      // The person is not logged into Facebook, so we're not sure if
+                      // they are logged into this app or not.
+                      document.getElementById('status').innerHTML = 'Please log ' +
+                        'into Facebook.';
+                    }
+                  }
+
+                  // This function is called when someone finishes with the Login
+                  // Button.  See the onlogin handler attached to it in the sample
+                  // code below.
+                  function checkLoginState() {
+                    FB.getLoginStatus(function(response) {
+                      statusChangeCallback(response);
+                    });
+                  }
+
+
+                  window.fbAsyncInit = function() {
+                  FB.init({
+                    appId      : '1738237216431523',
+                    cookie     : true,  // enable cookies to allow the server to access 
+                                        // the session
+                    xfbml      : true,  // parse social plugins on this page
+                    version    : 'v2.5' // use graph api version 2.5
+                  });
+
+                  FB.getLoginStatus(function(response) {
+                    statusChangeCallback(response);
+                  });
+
+                  };
+
+
+
+                  // Load the SDK asynchronously
+                  (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/en_US/sdk.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                  }(document, 'script', 'facebook-jssdk'));
+
+                  // Here we run a very simple test of the Graph API after login is
+                  // successful.  See statusChangeCallback() for when this call is made.
+                  function testAPI() {
+                    console.log('Welcome!  Fetching your information.... ');
+                    FB.api('/me', function(response) {
+                      console.log('Successful login for: ' + response.name);
+                    //  window.location = "http://localhost/uploader/index.php";    
+                      document.getElementById('status').innerHTML =
+                       'Thanks for logging in, ' + response.name + '!';
+                    });
+                  }
+    
+            </script>
 
             <!-- Bootstrap core JavaScript
     ================================================== -->
