2d5604d5c1e5cb7b5e56f9e66bc8a1275c02ca90
Debashis <rilu4u@gmail.com>
2016-06-30 18:48:42 +0530

    landing pages

---
+++ lp.php
@@ -0,0 +1,119 @@
+
+
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="Spottr is a way to discover the best and the latest companies providing everyday services that you can fall in love with.">
+        <!--<meta name="author" content="">-->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link href="bootstrap.min.css" rel="stylesheet">
+        <link href="jumbotron.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Abel|EB+Garamond|Exo|Josefin+Sans|Lato|Lobster|Montserrat|Oswald|PT+Sans|Poiret+One|Quicksand|Raleway|Roboto|Roboto+Condensed|Source+Sans+Pro|Ubuntu|Varela+Round" rel="stylesheet">
+        <!--Favicon-->
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        <link rel="canonical" href="www.spottr.in" />
+
+        <!-- CODE FOR FACEBOOK OPEN GRAPH-->
+        <meta property="og:title" content="Hello, Spottrs!" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="http://spottr.in" />
+        <meta property="og:image" content="http://spottr.in/spottr.png" />
+        <meta property="og:site_name" content="Spottr" />
+
+        <meta property="og:description" content="Spottr is a way to discover the best and the latest companies providing everyday services that you can fall in love with. Happy spotting!" />
+
+        <title>Spottr</title>
+
+           <script>
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-79507156-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+
+    </head>
+
+<body>
+        
+    <div class="jumbotron lp">
+        <img src="spottrlogo3.svg"></img>
+        <br>
+        <br>
+       <h4> Discover useful companies.</h4>
+
+    <br>
+        No more wasting hours, searching and asking around. <br>
+    Find all the relevant companies in one place. <br>
+    Less hassle. Better decisions.
+    <br>
+    <br>
+    
+    <img src="pic.png"></img>
+    </div>
+    
+    <div class="row lpage">
+        <div class="btn btn-primary">Rent a vehicle</div> 
+        <div class="btn btn-primary">Rental Accomodation / PG</div>
+        <div class="btn btn-primary">Rent Furniture</div>
+    </div>
+    
+  <div class="row lpage">
+      <img src="bottom.png"></img>
+<br><span style="font-size: 80%;">&copy; All rights reserved.</span>
+</div>
+
+    <style>
+        .lp{
+            width: 100%;
+            text-align: center;
+            color: black;
+        }
+        .lpage{
+            text-align: center;
+            background-color: white;
+            margin: 15px;
+            
+     
+        }
+        .btn{
+            margin: 10px;
+        }
+    </style>
+    
+</body>
+
+</html>
